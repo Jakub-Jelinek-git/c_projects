d82d405bbc2e764685a3532ab9b1121d68d50412
BigNomNom <jakub2007@atlas.cz>
2024-05-19 12:10:35 +0200

    mario project

---
+++ mario-more/mario.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 1; i <= height; i++)
+    {
+        int spaces = height - i;
+        for (int j = 1; j <= spaces; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int l = 1; l <= i; l++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
